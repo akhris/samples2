f527a8cbaf6a5085be24b4f372532278a6ce1597
Anatoly Khristianovsky <khristianovsky@gmail.com>
2022-08-18 09:12:03 +0300

    -added scaffold with top bar and navigation rail

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,17 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
+
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import ui.RootUi
 import ui.screens.nav_host.INavHost
 import ui.screens.nav_host.NavHostComponent
 import ui.screens.nav_host.NavHostUi
@@ -20,7 +16,7 @@ import ui.screens.nav_host.NavHostUi
 @Preview
 fun App(rootComponent: INavHost) {
     MaterialTheme {
-        NavHostUi(rootComponent)
+        RootUi(rootComponent)
     }
 }
 
--- src/jvmMain/kotlin/ui/RootUi.kt
+++ src/jvmMain/kotlin/ui/RootUi.kt
@@ -4,23 +4,62 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ui.screens.nav_host.INavHost
 import ui.screens.nav_host.NavHostUi
 
 @Composable
-fun RootUi(component: INavHost){
-    Row(modifier = Modifier.background(MaterialTheme.colors.background)) {
-//        NavigationRailUi(NavigationRailComponent(onNavigateTo = {
-//            component.setDestination(it.route)
-//        }, onAddButtonClicked = {
-//            addClickedNavItem = it
-//        }), localizedStrings = localizedStrings)
+fun RootUi(component: INavHost) {
 
-        Box(modifier = Modifier.fillMaxHeight().weight(1f)) {
-            NavHostUi(component = component)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed, confirmStateChange = { false })
+    val scaffoldState = rememberScaffoldState(drawerState = drawerState)
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar { Text("title") }
+        },
+        content = {
+            Row {
+                NavigationRail {
+                    NavigationRailItem(
+                        selected = true,
+                        icon = { Icons.Rounded.Person },
+                        label = { Text("item1") },
+                        onClick = {})
+                    NavigationRailItem(
+                        selected = false,
+                        icon = { Icons.Rounded.Email },
+                        label = { Text("item2") },
+                        onClick = {})
+                    NavigationRailItem(
+                        selected = false,
+                        icon = { Icons.Rounded.Check },
+                        label = { Text("item3") },
+                        onClick = {})
+                }
+                Box(modifier = Modifier.weight(1f)) { NavHostUi(component = component) }
+            }
         }
-    }
+    )
+
+//    Row(modifier = Modifier.background(MaterialTheme.colors.background)) {
+//
+////        NavigationRailUi(NavigationRailComponent(onNavigateTo = {
+////            component.setDestination(it.route)
+////        }, onAddButtonClicked = {
+////            addClickedNavItem = it
+////        }), localizedStrings = localizedStrings)
+//
+//        Box(modifier = Modifier.fillMaxHeight().weight(1f)) {
+//
+//        }
+//    }
 }
