4c23bbd48fff821abe2ae97d14aab0cc2bde4d96
Anatoly Khristianovsky <khristianovsky@gmail.com>
2022-09-07 12:49:07 +0300

    - added export to xls - added duplication - updated Compose and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
             packageName = "samples2"
             packageVersion = "1.0.0"
-            modules("java.instrument", "jdk.unsupported")
+            modules("java.instrument", "java.security.jgss", "java.sql", "java.xml.crypto", "jdk.unsupported")
         }
     }
 }
