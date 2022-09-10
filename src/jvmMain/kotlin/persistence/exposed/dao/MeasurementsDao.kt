7b7ee937645965e79ec4b9e459257406751e3cc0
Anatoly Khristianovsky <khristianovsky@gmail.com>
2022-09-10 21:38:59 +0300

    - fixed MeasurementsDao

--- src/jvmMain/kotlin/persistence/exposed/dao/MeasurementsDao.kt
+++ src/jvmMain/kotlin/persistence/exposed/dao/MeasurementsDao.kt
@@ -1,9 +1,16 @@
 package persistence.exposed.dao
 
 import domain.Measurement
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.statements.BatchUpdateStatement
 import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.statements.UpdateStatement
+import org.jetbrains.exposed.sql.update
 import persistence.exposed.dto.EntityMeasurement
+import persistence.exposed.dto.EntityMeasurementResult
 import persistence.exposed.dto.Tables
 import persistence.toMeasurement
 import utils.toUUID
@@ -21,7 +28,6 @@ class MeasurementsDao : BaseExposedDao<Measurement, EntityMeasurement, Tables.Me
         it[table.comment] = entity.comment
         it[table.conditions] = entity.conditions
         it[table.dateTime] = entity.dateTime
-        it[table.results] = entity.results
     }
 
     override fun insertStatement(entity: Measurement): Tables.Measurements.(InsertStatement<Number>) -> Unit = {
@@ -31,7 +37,36 @@ class MeasurementsDao : BaseExposedDao<Measurement, EntityMeasurement, Tables.Me
         it[table.comment] = entity.comment
         it[table.conditions] = entity.conditions
         it[table.dateTime] = entity.dateTime
-        it[table.results] = entity.results
+    }
+
+    override fun Transaction.doAfterUpdate(entity: Measurement) {
+        entity
+            .results
+            .forEach { mr ->
+                Tables
+                    .MeasurementResults
+                    .update(where = { Tables.MeasurementResults.measurement eq entity.id.toUUID() and (Tables.MeasurementResults.parameter eq mr.parameter.id.toUUID()) }) {
+                        it[value] = mr.value
+                        it[unit] = mr.unit
+                    }
+            }
+
+    }
+
+    override fun Transaction.doAfterInsert(entity: Measurement) {
+        entity
+            .results
+            .forEach { mr ->
+                Tables
+                    .MeasurementResults
+                    .insert {
+                        it[value] = mr.value
+                        it[unit] = mr.unit
+                        it[measurement] = entity.id.toUUID()
+                        it[parameter] = mr.parameter.id.toUUID()
+                    }
+            }
+
     }
 
 }
